cd74e826b82cd8bf331951eae073bb6818ea6428
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-07-24 14:49:18 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :orders, only: %i[index show] do
+    resources :packages, only: %i[new create]
+    resources :items, only: %i[new create]
+  end
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
