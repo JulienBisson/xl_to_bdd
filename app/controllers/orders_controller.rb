be00eec3649192cdf184ad4f12786f29b473aecf
Julien Bisson <130975079+JulienBisson@users.noreply.github.com>
2023-07-24 17:10:39 +0200

    display order 1

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,33 @@
+require 'rubyXL'
+require 'rubyXL/convenience_methods/cell'
+require 'rubyXL/convenience_methods/color'
+require 'rubyXL/convenience_methods/font'
+require 'rubyXL/convenience_methods/workbook'
+require 'rubyXL/convenience_methods/worksheet'
+
+class OrdersController < ApplicationController
+
+  def index
+    # Extraire les données du fichier Excel
+    workbook = RubyXL::Parser.parse('lib/assets/Orders.xlsx')
+    # Initialiser les variables
+    @orders = []
+    @packages = []
+    @items = []
+    order = []
+    # Parcourir "Order 1" du fichier Excel
+    order1 = workbook[0]
+
+    # Parcourir les lignes de "Order 1"
+    order1.each { |row|
+      @packages << row[0].value
+      @items << row[1].value
+      # Croisement horizontal et vertical des données pour avoir la valeur de chaque cellule
+      row && row.cells.each { |cell|
+        val = cell && cell.value
+        @orders << val
+      }
+    }
+    end
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,4 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0 }, presence: true
   validates :ref, uniqueness: true, numericality: true, presence: true
-  validates :warranty, inclusion: { in: [true, false] }
-  validates :duration, numericality: { greater_than: 0 }, presence: true
 end
